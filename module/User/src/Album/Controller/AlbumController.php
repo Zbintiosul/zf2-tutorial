6adef919df7e01db0213e9d61acfe4a26011d573
root <root@yopeso.com>
2014-08-25 18:17:39 +0300

    Added skeleton for user module

---
+++ module/User/src/Album/Controller/AlbumController.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mracu
+ * Date: 8/22/14
+ * Time: 10:35 AM
+ */
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Album\Model\Album;
+use Album\Form\AlbumForm;
+
+class AlbumController extends AbstractActionController
+{
+    protected $albumTable;
+    protected $genreTable;
+
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
+    }
+
+    public function getGenreTable()
+    {
+        if (!$this->genreTable) {
+            $sm = $this->getServiceLocator();
+            $this->genreTable = $sm->get('Album\Model\GenreTable');
+        }
+        return $this->genreTable;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
+    }
+
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->get('genre_id')->setValueOptions($this->getGenreTable()->getSelectArrayGenres());
+//        $form->get('genre_id')->setMessages(array('notInArray' => 'Please select Genre !'));
+//        //$form->get('genre_id')->setDisableInArrayValidator(true);
+//        //$form->get('genre_id')->Option('messages' ,array('notInArray' => 'Please select Genre !'));
+//        die(print_r($form->get('genre_id')->getInputSpecification()));
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+                $this->getAlbumTable()->saveAlbum($album);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+        return array('form' => $form);
+    }
+
+    public function editAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album', array(
+                'action' => 'add'
+            ));
+        }
+
+        // Get the Album with the specified id.  An exception is thrown
+        // if it cannot be found, in which case go to the index page.
+        try {
+            $album = $this->getAlbumTable()->getAlbum($id);
+        }
+        catch (\Exception $ex) {
+            return $this->redirect()->toRoute('album', array(
+                'action' => 'index'
+            ));
+        }
+        $form  = new AlbumForm();
+        $form->bind($album);
+
+        $form->get('genre_id')->setValueOptions($this->getGenreTable()->getSelectArrayGenres());
+        $form->get('submit')->setAttribute('value', 'Edit');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setInputFilter($album->getInputFilter());
+            //die(print_r($request->getPost()));
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $this->getAlbumTable()->saveAlbum($album);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
+    }
+
+    public function deleteAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album');
+        }
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->getPost('del', 'No');
+
+            if ($del == 'Yes') {
+                $id = (int) $request->getPost('id');
+                $this->getAlbumTable()->deleteAlbum($id);
+            }
+
+            // Redirect to list of albums
+            return $this->redirect()->toRoute('album');
+        }
+
+        return array(
+            'id'    => $id,
+            'album' => $this->getAlbumTable()->getAlbum($id)
+        );
+    }
+
+
+}
---
+++ module/User/src/Album/Model/AlbumTable.php
@@ -0,0 +1,62 @@
+<?php
+
+ namespace Album\Model;
+
+ use Zend\Db\TableGateway\TableGateway;
+
+ class AlbumTable
+ {
+     protected $tableGateway;
+
+     public function __construct(TableGateway $tableGateway)
+     {
+         $this->tableGateway = $tableGateway;
+     }
+
+     public function fetchAll(Array $columns = array('id','title', 'artist'))
+     {
+         $sqlSelect = $this->tableGateway->getSql()->select();
+         $sqlSelect->columns($columns);
+         $sqlSelect->join('genres', 'album.genre_id = genres.id', array('genre'=>'name'), 'left');
+
+         $resultSet = $this->tableGateway->selectWith($sqlSelect);
+         return $resultSet;
+     }
+
+
+     public function getAlbum($id)
+     {
+         $id  = (int) $id;
+         $rowset = $this->tableGateway->select(array('id' => $id));
+         $row = $rowset->current();
+         if (!$row) {
+             throw new \Exception("Could not find row $id");
+         }
+         return $row;
+     }
+
+     public function saveAlbum(Album $album)
+     {
+         $data = array(
+             'artist' => $album->artist,
+             'title'  => $album->title,
+             'genre_id'  => $album->genre_id,
+         );
+
+         $id = (int) $album->id;
+         if ($id == 0) {
+             $this->tableGateway->insert($data);
+         } else {
+             if ($this->getAlbum($id)) {
+                 $this->tableGateway->update($data, array('id' => $id));
+             } else {
+                 throw new \Exception('Album id does not exist');
+             }
+         }
+     }
+
+     public function deleteAlbum($id)
+     {
+         $this->tableGateway->delete(array('id' => (int) $id));
+     }
+ }
